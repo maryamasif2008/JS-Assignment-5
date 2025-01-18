899c76fe6dee30d7ecb55d660233f6c6b63a31ec
maryamasif2008 <marayam.asif123@gmail.com>
2025-01-19 01:48:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+                                             //Chapter 05//
+
+                                           /* Question 01 */
+var num1 = 5;
+var num2 = 8;                                         
+    
+var sum = num1 + num2;
+document.getElementById("result").innerHTML = `The sum of ${num1} and ${num2} is:`
+
+                                           /* Question 02 */
+var value1 = 10;
+var value2 = 5;
+
+// Addition
+let addition = value1 + value2;
+
+// Subtraction
+let subtraction = value1 - value2;
+
+// Multiplication
+let multiplication = value1 * value2;
+
+// Division
+let division = value1 / value2;
+
+// Modulus (remainder of division)
+let modulus = value1 % value2;
+
+document.getElementById("addition-result").innerHTML = `The sum of ${value1} and ${value2} is: ${addition}`;
+document.getElementById("subtraction-result").innerHTML = `The difference of ${value1} and ${value2} is: ${subtraction}`;
+document.getElementById("multiplication-result").innerHTML = `The product of ${value1} and ${value2} is: ${multiplication}`;
+document.getElementById("division-result").innerHTML = `The division of ${value1} by ${value2} gives: ${division}`;
+document.getElementById("modulus-result").innerHTML = `The remainder of ${value1} divided by ${value2} is: ${modulus}`;  
+
+                                         /* Question 03 */
+// a. 
+var myVariable;
+
+// b. 
+document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+// c. 
+myVariable = 5;
+
+// d. 
+document.write("Initial value: " + myVariable + "<br>");
+
+// e. 
+myVariable++;
+
+// f. 
+document.write("Value after increment is: " + myVariable + "<br>");
+
+// g. 
+myVariable += 7;
+
+// h. 
+document.write("Value after addition is: " + myVariable + "<br>");
+
+// i.
+myVariable--;
+
+// j. 
+document.write("Value after decrement is: " + myVariable + "<br>");
+
+// k. 
+let remainder = myVariable % 3;
+
+// l. 
+document.write("The remainder is: " + remainder + "<br>" +"<br>");
+                                         
+                                        /* Question 04 */
+var ticketPrice = 600;
+var totalCost = ticketPrice * 5;
+
+document.write("The cost of buying 5 movie tickets is: " + totalCost + " PKR" + "<br>");
+                                        
+                                        /* Question 05 */
+var message = "<h3> Table of 4 </h3> \n 4x1=4 <br> 4x2=8 <br> 4x3=12 <br> 4x4=16 <br> 4x5=20 <br> 4x6=24 <br> 4x7=28 <br> 4x8=32 <br> 4x9=36 <br> 4x10=40 <br> <br>";
+document.write(message)
+
+                                        /* Question 06 */
+var message = "25<sup>0</sup>C is 77<sup>0</sup>F <br> 70<sup>0</sup>F is 21.11111111111111<sup>0</sup>C ";
+  document.write(message)                                         
+
+                                       /* Question 07 */
+var message = "<h2> Shopping Cart </h2> ";
+document.write(message) 
+                         
+var message = " Price of item 1 is ";
+document.write(message) 
+                       
+var item1= 650;
+document.write(item1) 
+                                          
+var message = " <br> Quality of item 1 is ";
+document.write(message) 
+                       
+var item1= 3;
+document.write(item1) 
+                       
+var message = " <br> Price of item 2 is ";
+document.write(message) 
+                       
+var item2= 100;
+document.write(item2) 
+                       
+var message = " <br> Quality of item 2 is ";
+document.write(message) 
+                       
+var item2= 7;
+document.write(item2) 
+                       
+var message = " <br> <br>  Total cost of order is ";
+document.write(message) 
+                       
+var total= 2750;
+document.write(total) 
+                       
+                                        /* Question 08 */
+var message = "<h2>Marks Sheet</h2> ";
+document.write(message) 
+                        
+var message = " \n Total marks: 980 <br> Markobtained: 804  ";
+document.write(message) 
+                         
+var message = " Totalmarks: 980 <br> Marksobtained: 804 <br> Percentage: 82.0408163265306%  ";
+document.write(message)   
+
+                                      /* Question 09 */
+var message = "<h2>Currency in PKR</h2> ";
+document.write(message) 
+                                                              
+var message = " Total Currency in PKR: 1748 ";
+document.write(message) 
+                                                           
+                                      /* Question 10 */
+var message = " <h1>Question 10</h1> ";
+document.write(message) 
+                                        
+var initial_value = 8; 
+document.write( (initial_value + 5) * 10 / 2)
+                       
+                                      /* Question 11 */
+var message = "<h2>Age Calculator</h2> ";
+document.write(message) 
+                                                                               
+var message = "Current year: 2024 <br> Birth year: 2008 <br> Your age is: 17 ";
+document.write(message) 
+                                                            
+                                      /* Question 12 */
+var message = "<h2>The Geometrizer</h2> ";
+document.write(message) 
+                                                                             
+var message = "Radius of a circle: 20 <br> The circumference: 125.6799999999999 <br> The area is: 1256.8 ";
+document.write(message) 
+                                        
+                                     /* Question 13 */
+var message = " <h2>The Liftime Supply Calculator</h2> ";
+document.write(message) 
+                                                                             
+var message = "Favourite Snake: Chocolate <br> Current age: 16 <br> Extimated Mximum Age: 66 <br> Amount of snake per day: 3 <br> You will need 150 chocolate to last you until the rip old age of 65 ";
+document.write(message) 
+
+                      
